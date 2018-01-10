706cefac93388a6552ae30e63faa01fb221ce81f
duxiaodong <duxiaodong@darlin.me>
2018-01-10 22:46:52 +0800

    only generate and echo index.html

--- build-samples.sh
+++ build-samples.sh
@@ -4,19 +4,23 @@ set -o errexit
 
 echo 'Building sample/hello-world!'
 cd samples/hello-world
-../../bin/fec-builder build -e production
+../../bin/fec-builder clean && ../../bin/fec-builder generate -e production
+echo "$(cat dist/index.html)"
 
 echo 'Building sample/react!'
 cd ../react
 npm i
-../../bin/fec-builder build -e production
+../../bin/fec-builder clean && ../../bin/fec-builder generate -e production
+echo "$(cat dist/index.html)"
 
 echo 'Building sample/react-mobx-ssr!'
 cd ../react-mobx-ssr
 npm i
 ../../bin/fec-builder clean && ../../bin/fec-builder generate -e production --ISOMORPHIC_TOOLS_FILE ./server/isomorphic.js
+echo "$(cat dist/index.html)"
 
 echo 'Building sample/typescript-react!'
 cd ../typescript-react
 npm i 
-../../bin/fec-builder build -e production
+../../bin/fec-builder clean && ../../bin/fec-builder generate -e production
+echo "$(cat dist/index.html)"
