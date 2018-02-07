d7025dd114c3156a8537e26fd757299a8506e158
darlin <duxiaodong@darlin.me>
2018-02-07 09:38:30 +0800

    update build-samples.sh

--- build-samples.sh
+++ build-samples.sh
@@ -2,25 +2,19 @@
 
 set -o errexit
 
-echo 'Building sample/hello-world!'
-cd samples/hello-world
-../../bin/fec-builder clean && ../../bin/fec-builder generate -e production
-echo "$(cat dist/index.html)"
+sudo npm link
 
-echo 'Building sample/react!'
-cd ../react
-npm i
-../../bin/fec-builder clean && ../../bin/fec-builder generate -e production
-echo "$(cat dist/index.html)"
-
-echo 'Building sample/react-mobx-ssr!'
-cd ../react-mobx-ssr
-npm i
-../../bin/fec-builder clean && ../../bin/fec-builder generate -e production --ISOMORPHIC_TOOLS_FILE ./server/isomorphic.js
-echo "$(cat dist/index.html)"
-
-echo 'Building sample/typescript-react!'
-cd ../typescript-react
-npm i 
-../../bin/fec-builder clean && ../../bin/fec-builder generate -e production
-echo "$(cat dist/index.html)"
+cd samples
+for dir in `ls .`
+do
+  if [ -d $dir ]; then
+    echo "Prepare samples/$dir"
+    cd $dir
+    if [ -f fec-test.sh ]; then
+      . fec-test.sh
+    else
+        echo "Skip the samples/$dir"
+    fi
+    cd ..
+  fi
+done
