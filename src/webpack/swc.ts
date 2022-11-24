12a612ebb8b1fb384ff65ad703bcc55b4a08e45d
liaoyu <liaoyu753@163.com>
2022-11-24 17:35:11 +0800

    swc support (#167)
    * feat: init swc
    
    * feat: swc integrate
    
    * fix: compat mobx
    
    * fix: compat browserslist
    
    * fix: detect babel options
    
    * fix: check swc should enabled
    
    * fix: add shippedProposals
    
    * fix: convert tsconfig.json to swc config
    
    * fix: fix review & update builder-config.md
    
    Co-authored-by: liaoxiaoyou <liaoxiaoyou@youzan.com>

---
+++ src/webpack/swc.ts
@@ -0,0 +1,111 @@
+import fs from 'fs'
+import browserslist from 'browserslist'
+import { Options as SwcOptions } from '@swc/core'
+import { CompilerOptions } from 'typescript'
+import { mergeWith } from 'lodash'
+
+import { shouldAddGlobalPolyfill, AddPolyfill } from '../utils/build-conf'
+import { abs } from '../utils/paths'
+
+/** 读取 tsconfig.json 文件获取 compilerOptions 配置 */
+function getTsCompilerOptions() {
+  const filePath = abs('tsconfig.json')
+
+  if (fs.existsSync(filePath)) {
+    const rawContent = fs.readFileSync(filePath, { encoding: 'utf8' })
+    const jsonContent = JSON.parse(rawContent)
+    return jsonContent?.compilerOptions as CompilerOptions
+  }
+
+  return null
+}
+
+/** swc 不会读取 tsconfig.json 的配置，这里手动转成 swc 的配置 */
+/** 参考自 https://github.com/Songkeys/tsconfig-to-swcconfig/blob/62e7f585882443bd27beb5b2e05a680f18070198/src/index.ts */
+function transformTsCompilerOptions(options: CompilerOptions): SwcOptions {
+  const {
+    importHelpers = false,
+    experimentalDecorators = false,
+    emitDecoratorMetadata = false,
+    jsxFactory = 'React.createElement',
+    jsxFragmentFactory = 'React.Fragment',
+    jsxImportSource = 'react',
+    paths,
+    baseUrl
+  } = options
+
+  return {
+    jsc: {
+      externalHelpers: importHelpers,
+      parser: {
+        syntax: 'typescript',
+        decorators: experimentalDecorators
+      },
+      transform: {
+        decoratorMetadata: emitDecoratorMetadata,
+        react: {
+          pragma: jsxFactory,
+          pragmaFrag: jsxFragmentFactory,
+          importSource: jsxImportSource,
+        },
+      },
+      paths,
+      baseUrl
+    }
+  }
+}
+
+export function makeSwcLoaderOptions(
+  /** https://swc.rs/docs/configuration/supported-browsers#targets */
+  targets: string[],
+  /** polyfill 模式 */
+  polyfill: AddPolyfill,
+  /** 是否 react 项目 */
+  withReact = false,
+  /** 是否 ts 语法 */
+  isTsSyntax = false
+): SwcOptions {
+  const swcOptions: SwcOptions = {
+    jsc: {
+      parser: {
+        syntax: isTsSyntax ? 'typescript' : 'ecmascript',
+        jsx: withReact,
+        dynamicImport: true,
+        decorators: true
+      },
+      transform: {
+        legacyDecorator: true,
+        decoratorMetadata: true,
+        useDefineForClassFields: true
+      },
+      externalHelpers: true
+    },
+    env: {
+      targets: browserslist(targets),
+      ...(
+        // global polyfill
+        shouldAddGlobalPolyfill(polyfill)
+        && {
+          // https://swc.rs/docs/configuration/supported-browsers#mode
+          mode: 'usage',
+          coreJs: '3',
+          shippedProposals: true
+        }
+      )
+    }
+  }
+
+  if (isTsSyntax) {
+    const compilerOptions = getTsCompilerOptions()
+
+    if (compilerOptions != null) {
+      return mergeWith(swcOptions, transformTsCompilerOptions(compilerOptions), (_srcValue, targetValue) => {
+        if (Array.isArray(targetValue)) {
+          return targetValue
+        }
+      })
+    }
+  }
+
+  return swcOptions
+}
