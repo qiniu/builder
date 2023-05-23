6eae30cdffb54e63bab713c818b080c55ef3271f
liaoyu <liaoyu753@163.com>
2023-05-23 17:32:33 +0800

    fix: 开启 swc 情况下解析 tsconfig.json 文件内容含有注释会报错问题 (#172)
    * fix: swc parse tsconfig
    
    * chore: version
    
    * chore: swc parse
    
    * chore: upgrade postcss
    
    * chore: release 2.7.0
    
    * fix: swc parse
    
    * fix: typo
    
    * fix: restart parse tsconfig.json
    
    * fix: self review
    
    * fix: typo
    
    * fix: mergeWith
    
    ---------
    
    Co-authored-by: liaoxiaoyou <liaoxiaoyou@youzan.com>

--- src/webpack/swc.ts
+++ src/webpack/swc.ts
@@ -1,20 +1,48 @@
 import fs from 'fs'
+import path from 'path'
 import browserslist from 'browserslist'
 import { Options as SwcOptions } from '@swc/core'
 import { CompilerOptions } from 'typescript'
 import { mergeWith } from 'lodash'
+import { parse } from 'jsonc-parser'
 
 import { shouldAddGlobalPolyfill, AddPolyfill } from '../utils/build-conf'
-import { abs } from '../utils/paths'
+import { getBuildRoot } from '../utils/paths'
 
 /** 读取 tsconfig.json 文件获取 compilerOptions 配置 */
-function getTsCompilerOptions() {
-  const filePath = abs('tsconfig.json')
+function getTsCompilerOptions(): CompilerOptions | null {
+  const { compilerOptions } = loadTsConfig('tsconfig.json') ?? {}
+  return compilerOptions ?? null
+}
+
+function loadTsConfig(filename: string, tsConfig?: any): any {
+  let data = resolveTsConfig(filename) ?? {}
+
+  if (tsConfig) {
+    data = mergeWith(data, tsConfig, (_srcValue, targetValue) => {
+      if (Array.isArray(targetValue)) {
+        return targetValue
+      }
+    })
+  }
+
+  let extendsName = data.extends
+  if (!extendsName) return data
+
+  delete data.extends
+  if (!extendsName.endsWith('.json')) {
+    extendsName += '.json'
+  }
+
+  return loadTsConfig(extendsName, data)
+}
+
+function resolveTsConfig(filename: string) {
+  const filePath = path.resolve(getBuildRoot(), filename)
 
   if (fs.existsSync(filePath)) {
-    const rawContent = fs.readFileSync(filePath, { encoding: 'utf8' })
-    const jsonContent = JSON.parse(rawContent)
-    return jsonContent?.compilerOptions as CompilerOptions
+    const content = fs.readFileSync(filePath, { encoding: 'utf8' })
+    return parse(content)
   }
 
   return null
@@ -22,7 +50,7 @@ function getTsCompilerOptions() {
 
 /** swc 不会读取 tsconfig.json 的配置，这里手动转成 swc 的配置 */
 /** 参考自 https://github.com/Songkeys/tsconfig-to-swcconfig/blob/62e7f585882443bd27beb5b2e05a680f18070198/src/index.ts */
-function transformTsCompilerOptions(options: CompilerOptions): SwcOptions {
+function transformTsCompilerOptions(options: CompilerOptions | null): SwcOptions {
   const {
     importHelpers = false,
     experimentalDecorators = false,
@@ -32,7 +60,7 @@ function transformTsCompilerOptions(options: CompilerOptions): SwcOptions {
     jsxImportSource = 'react',
     paths,
     baseUrl
-  } = options
+  } = options ?? {}
 
   return {
     jsc: {
